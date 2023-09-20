9e085a6a5d5b45ce1c8080543a7f4b519586b3bd
Hassaan-Coder <hassaansultan720@gmail.com>
2023-09-20 11:24:45 +0500

    Update chanages of NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
@@ -9,16 +10,10 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
       <main className="relative flex-1 lg:container text-center p-0 mx-auto overflow-x-hidden overflow-auto">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        …
       </main>
-
-      {/* <Footer /> */}
     </div>
   );
 };
