14c69edf083f26933e962241e847f5a85bad8ada
Hassaan-Coder <hassaansultan720@gmail.com>
2023-09-20 15:12:54 +0500

    delete unused vars

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero from "../components/hero";
@@ -16,7 +15,6 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      {/* <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center"> */}
       <main className="relative flex-1 lg:container text-center p-0 mx-auto overflow-x-hidden overflow-auto">
         <Hero />
         <Goal />
